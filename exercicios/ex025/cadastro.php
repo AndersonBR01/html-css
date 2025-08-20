ffb3fce793722dd59ba4f6819779a35d2a4bbd48
ANDERSON CARLOS SILVA <andersoncsilva850@gmail.com>
2025-08-20 09:44:53 -0300

    formulário 003

---
+++ exercicios/ex025/cadastro.php
@@ -0,0 +1,6 @@
+<?php
+// Exemplo básico de PHP
+
+echo "Olá, mundo! Este é um script PHP básico.";
+echo "<br>Este é um exemplo de como o PHP pode ser usado para gerar conteúdo dinâmico em uma página web.";
+?>
